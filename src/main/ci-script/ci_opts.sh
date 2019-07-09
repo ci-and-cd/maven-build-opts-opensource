bb30f65d493905b0ec757d15a6794a55e5574768
haolun zhang <chshawkn@users.noreply.github.com>
2019-07-09 21:52:11 +0800

    update variable names

--- src/main/ci-script/ci_opts.sh
+++ src/main/ci-script/ci_opts.sh
@@ -1,11 +1,11 @@
 # no shebang line here
 
-if [ -z "${CI_OPT_OPENSOURCE_GIT_PREFIX}" ]; then CI_OPT_OPENSOURCE_GIT_PREFIX="https://github.com"; fi
+if [ -z "${CI_OPT_OSSRH_GIT_PREFIX}" ]; then CI_OPT_OSSRH_GIT_PREFIX="https://github.com"; fi
 # dockerfile-maven-plugin's default docker registry is 'docker.io'. docker hub: https://docker.io/v2/ or https://registry-1.docker.io/v2/
 if [ -z "${CI_OPT_DOCKER_REGISTRY_URL}" ]; then CI_OPT_DOCKER_REGISTRY_URL="https://docker.io/v2/"; fi
 # infrastructure oepnsource use maven central, CI_OPT_NEXUS3 is only for maven-site.
-if [ -z "${CI_OPT_OPENSOURCE_NEXUS3}" ]; then CI_OPT_OPENSOURCE_NEXUS3="https://nexus3.infra.top/nexus/"; fi
-# infrastructure oepnsource use https://sonarqube.com, CI_OPT_OPENSOURCE_SONAR_HOST_URL is not needed.
+if [ -z "${CI_OPT_OSSRH_NEXUS3}" ]; then CI_OPT_OSSRH_NEXUS3="https://nexus3.infra.top/nexus/"; fi
+# infrastructure oepnsource use https://sonarqube.com, CI_OPT_OSSRH_SONAR_HOST_URL is not needed.
 
 if [ -z "${CI_OPT_CHECKSTYLE_CONFIG_LOCATION}" ]; then CI_OPT_CHECKSTYLE_CONFIG_LOCATION="${CI_OPT_MAVEN_BUILD_REPO}/src/main/checkstyle/google_checks_8.10.xml"; fi
 # default: https://nodejs.org/dist/  taobao: https://npm.taobao.org/mirrors/node/"
